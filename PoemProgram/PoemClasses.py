1e71c534976cbdfbd3736016fd6aede198e89252
GensokyoLRE <31522695+GensokyoLRE@users.noreply.github.com>
2018-10-31 21:43:12 -0700

    HalloweenChange
    Minor Cleanup. Will take penalty for cleanliness, cause honorable death is a thing i guess. (~o3o)~

--- PoemProgram/PoemClasses.py
+++ PoemProgram/PoemClasses.py
@@ -1,9 +1,10 @@
 """
 Program: PoemClasses.py
 Author: Phillip "Hifumi" Cuesta
-Last Mod: 8:14 PM 10/30/18
+Last Mod: 8:14 PM 10/30/18 (text modded 9:41 PM 10/31/18 spoopy)
 Purpose: Read in a file that contains some form of poetry to a
 class and see if it's a haiku.
+Disclaimer: Only works if first line format is "NameOfPoem by NameOfAuthor".
 """
 import os
 
@@ -13,22 +14,48 @@ class Poem:
     _count = 0
 
     def __init__(self, file):
+        """
+        Intializes Poem class.
+        :param file: Takes in the file given to the class and reads to variable.
+        """
         self.lines = file
         Poem._count += 1
 
     def get_name(self):
+        """
+        Gets name/title of the poem.
+        :return: Name of poem.
+        """
         return self.name
 
     def get_author(self):
+        """
+        Gets author of poem.
+        :return: Author of poem.
+        """
         return self.author
 
     def set_name(self, name):
+        """
+        Lets user enter name of poem.
+        :param name: Name of poem.
+        :return: Nothing
+        """
         self.name = name
 
     def set_author(self, author):
+        """
+        Lets user enter author of poem.
+        :param author: Author of poem.
+        :return: Nothing
+        """
         self.author = author
 
     def get_count(self):
+        """
+        Gets count of the class usage.
+        :return: Count of usages.
+        """
         return self._count
 
     def is_haiku(self, file):
@@ -44,6 +71,9 @@ class Poem:
             is_hai = True
         if is_hai:
             print("\nIt may be a haiku. Running in-testing evaluation of syllables...")
+            """
+            The following is a vowel finder. Not a verified thing to work.
+            """
             syll = lambda x: len(''.join(" x"[c in "aeiouy"] for c in (x[:-1] if 'e' == x[-1] else x)).split())
             h_check = []
             for line in lines:
@@ -60,26 +90,29 @@ class Poem:
             return False
 
 
-while True:
-    try:
-        openFile = input("Enter a poem file name: ") + ".txt"
-        if not os.path.isfile(openFile):
-            openFile = "DefaultPoem.txt"
-            print("Defaulting to DefaultPoem\n")
-        fullFile = open(openFile, 'r').read()
-        poemType = Poem(fullFile)
-        poemLines = fullFile.splitlines()
-        aName, aAuthor = poemLines[0].split(" by ")
-        poemType.set_name(aName)
-        poemType.set_author(aAuthor)
-        print("Name: %s\n"
-              "Author: %s\n"
-              "Poem:\n" % (poemType.get_name(), poemType.get_author()))
-        poemType.is_haiku(fullFile)
-        cont = input("Do you wanna do another one? Y/N: ")
-        if cont.upper() == "N":
-            print("You checked %i poems. Thank you for using this program!" % poemType.get_count())
-            exit(0)
-    except Exception as e:
-        print("Error: %s" % e)
+def main():
+    while True:
+        try:
+            open_file = input("Enter a poem file name (Case Sensitive): ") + ".txt"
+            if not os.path.isfile(open_file):
+                open_file = "DefaultPoem.txt"
+                print("Defaulting to DefaultPoem\n")
+            full_file = open(open_file, 'r').read()
+            poem_type = Poem(full_file)
+            poem_lines = full_file.splitlines()
+            a_name, a_author = poem_lines[0].split(" by ")
+            poem_type.set_name(a_name)
+            poem_type.set_author(a_author)
+            print("Name: %s\n"
+                  "Author: %s\n"
+                  "Poem:\n" % (poem_type.get_name(), poem_type.get_author()))
+            poem_type.is_haiku(full_file)
+            cont = input("Do you wanna do another one? Y/N: ")
+            if cont.upper() == "N":
+                print("You checked %i poems. Thank you for using this program!" % poem_type.get_count())
+                exit(0)
+        except Exception as e:
+            print("Error: %s" % e)
 
+if __name__ == "__main__":
+    main()
